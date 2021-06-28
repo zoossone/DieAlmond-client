a64f56d4027f392ef883496f5b03ad3f347d3c74
Chu Sangwon <zoossone@gmail.com>
2021-06-28 10:31:01 +0900

    client setting

---
+++ src/components/AllBucketList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllBucketList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllBucketList;
---
+++ src/components/NaviBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NaviBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NaviBar;
