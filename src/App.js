37790fcb292b02f2b62c659e35fed8fca022853f
Kim SungJin <gotninja0102@gmail.com>
2021-06-29 10:27:00 +0900

    loading finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LandingPage from './Pages.js/LandingPage';
 
 function App() {
   return (
     <div>
-      Hello World~
+      <LandingPage />
     </div>
   );
 }
--- src/Pages.js/LandingPage.js
+++ src/Pages.js/LandingPage.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginModal from '../components/LoginModal';
+import NaviBar from '../components/NaviBar'
+import Footer from '../components/Footer'
+import styled from 'styled-components';
 
 const LandingPage = () => {
+    // 모달창 상태 여기서만 필요하니가 리덕스 굳이 안써도될듯 ㅇㅇ
+    const [login, setLogin] = useState(false)
+
+    const LoginModalBtn = styled.button`
+        background-color: #00FFFF;
+        text-decoration: none;
+        border: none;
+        padding: 10px;
+        color: black;
+        border-radius: 30px;
+        cursor: grab;
+    `;
+
+    const StartBtn = styled.button`
+        background-color: #00FFFF;
+        text-decoration: none;
+        border: none;
+        padding: 10px;
+        color: black;
+        border-radius: 30px;
+        cursor: grab;
+    `;
+
+    const handleLoginModal = () => {
+        setLogin(!login)
+    }
     return (
         <div>
-            
+            <NaviBar />
+            <header>
+                <h1>DieAlmond</h1>
+                <p>introduction</p>
+            </header>
+            <img src="#" />
+            <div>
+                <LoginModalBtn onClick={handleLoginModal}>로그인</LoginModalBtn>
+                {login === false ?
+                    null :
+                    <LoginModal handleLoginModal={handleLoginModal}/>
+                }
+                <StartBtn>시작하기</StartBtn>
+            </div>
+            <Footer/>
         </div>
     );
 };
