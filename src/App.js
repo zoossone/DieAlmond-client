dced1fa8e8603eb8ef8e344a38c8f481221960c9
Kim SungJin <gotninja0102@gmail.com>
2021-07-01 10:20:22 +0900

    oauth finish

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import LandingPage from './Pages/LandingPage';
 import { Route, Redirect } from "react-router-dom"
 import SettingModal from './components/SettingModal/SettingModal'
 import StartBtn from './components/StartBtn';
+import GooLogin from './components/oauth/GooLogin'
+import GooLogout from './components/oauth/GooLogout';
+import KakaoLogin from './components/oauth/KakaoLogin';
+import KakaoLogout from './components/oauth/KakaoLogout';
+import NaviBar from './components/NaviBar';
 
 function App() {
   return (
-    <Route>
+    <div>
       <Route exact path="/"
         render={() => (
           <LandingPage />
         )} />
       {/* <SettingModal /> */}
       <StartBtn />
-    </Route>
+      <Route exact path="/login"
+        render={() => (
+          <div>
+            <GooLogin />
+            <GooLogout />
+            <KakaoLogin />
+            <KakaoLogout />
+          </div>
+        )} />
+    </div>
   );
 }
 
