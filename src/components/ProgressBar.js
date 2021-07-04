abf5b6a0dd6d8f0a6481dd8325b7a516835f4ae1
Chu Sangwon <zoossone@gmail.com>
2021-07-05 01:24:36 +0900

    :hammer: progressbar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Progress } from 'semantic-ui-react'
 
 const ProgressBar = () => {
+    //props로 수명을 받아서 스테이트로 표시해줌
 
     const [ percent, setPercent ] = useState(50);
     
